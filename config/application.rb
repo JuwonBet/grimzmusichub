75e921514777b1357a2f4e8a2bfac3e36b893273
JuwonBet <geetechng@gmail.com>
2016-05-01 14:55:44 +0000

    config

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module GMH
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.serve_static_assets = true
+    config.serve_static_files = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
